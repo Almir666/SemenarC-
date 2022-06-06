dcb9deeb1f28fbfcf4466868c69936b9846c6e4f
Almir <crafteasy@yandex.ru>
2022-06-06 10:49:07 +0300

    Корень числа

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Напишите программу, которая навход принимает число и выдает его квадрат
+
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Число: " + n);
+Console.WriteLine("Квадрат числа: " + n * n);

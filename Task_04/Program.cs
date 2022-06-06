11764fc056ab28409263d145f9db7a4f4e743394
Almir <crafteasy@yandex.ru>
2022-06-06 12:25:35 +0300

    Трехзначное число недоделанная задача

---
+++ Task_04/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int xnumber = int.Parse(Console.Readline());
+
+Console.WriteLine (xnumber % 10);
